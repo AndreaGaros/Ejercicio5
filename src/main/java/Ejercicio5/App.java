0fffcd8feaba99ac92db0e2e89bd56d7fec25b3c
Andrea Garós <agarosmoyano@gmail.com>
2015-09-30 21:01:36 +0200

    varsión uno Ejercicio5

---
+++ src/main/java/Ejercicio5/App.java
@@ -0,0 +1,20 @@
+package Ejercicio5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) throws InterruptedException {
+        ThreadClass threadClass = new ThreadClass("thread class");
+        Thread thread = new Thread(new RunneableClass(), "runneable class");
+        threadClass.start();
+        thread.start();
+
+        threadClass.join();
+        thread.join();
+
+        System.out.println("Se acabó.");
+    }
+}
